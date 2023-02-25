3355c72641f2a8f14af7a5a7bf5567eab366f211
Tooneytunes <tjvandort@hotmail.com>
2023-02-25 18:00:00 +0100

    Webapp

--- findrecipe.py
+++ findrecipe.py
@@ -1,56 +1,67 @@
+import uvicorn
+from fastapi import FastAPI
+from jinja2 import Environment, FileSystemLoader
+from fastapi.responses import HTMLResponse
+
 # Open the recipe file and read its contents
 with open("recipe.txt", "r") as file:
     recipe_list = file.readlines()
+
 
 # Define a function to search for a given ingredient in a recipe
 def has_ingredient(recipe, ingredient):
     return ingredient.lower() in recipe.lower()
 
-# Ask the user for the ingredient they want to search for
-search_ingredient = input("Enter an ingredient to search for: ")
 
-# Loop through each recipe in the recipe list and store the title and instructions of any that contain the ingredient
-recipes = []
-seen_titles = set()  # Keep track of the titles we've already seen
-for i in range(len(recipe_list)):
-    if "Gerecht" in recipe_list[i]:  # The line containing the title has "Gerecht"
-        title_number = recipe_list[i].split()[1]  # Extract the number from the line
-        title = recipe_list[i].replace(f"Gerecht {title_number}:", "").strip()  # Extract the title from the line and remove whitespace
-    elif has_ingredient(recipe_list[i], search_ingredient) and title not in seen_titles:
-        seen_titles.add(title)
-        instructions = []  # Initialize a list to store the recipe instructions
-        for j in range(i+1, len(recipe_list)):
-            if "Gerecht" in recipe_list[j]:
-                recipes.append({"title": title, "instructions": instructions})  # Store the title and instructions in a dictionary and append it to the list of recipes
-                break
-            else:
-                instructions.append(recipe_list[j].strip())  # Add the line to the instructions
-    if len(recipes) == 5:  # Stop searching after the first 5 matches are found
-        break
-
-# Print the found options
-if len(recipes) == 0:
-    print("No recipes found containing", search_ingredient)
-else:
-    for i in range(len(recipes)):
-        print(f"{i+1}. {recipes[i]['title']}")
-
-    # Ask the user if they want to add any recipes to their boodschappenlijst
-    response = input("Would you like to add any of these recipes to your boodschappenlijst? (y/n)").lower()
-    if response == "y":
-        # Open the boodschappenlijst file in append mode
-        with open("boodschappenlijst.txt", "a") as file:
-            while True:
-                # Ask the user for the number of the recipe they want to add
-                recipe_num = input("Enter the number of the recipe you want to add to your boodschappenlijst (or 'done' to exit): ")
-                if recipe_num == "done":
+def find_ingredient(search_ingredient):
+    # Loop through each recipe in the recipe list and store the title and instructions of any that contain the ingredient
+    recipes = []
+    seen_titles = set()  # Keep track of the titles we've already seen
+    for i in range(len(recipe_list)):
+        if len(recipes) >= 5:  # Stop searching after the first 5 matches are found
+            break
+        if "Gerecht" in recipe_list[i]:  # The line containing the title has "Gerecht"
+            title = recipe_list[i].replace(f"Gerecht:", "").strip()  # Extract the title from the line and remove whitespace
+            start_line = i
+            amount = recipe_list[start_line + 2]
+        elif "Gehaald van:" in recipe_list[i]:
+            url = recipe_list[i].replace(f"Gehaald van:", "").strip()
+        elif (has_ingredient(recipe_list[i], search_ingredient) or has_ingredient(recipe_list[start_line], search_ingredient)) and title not in seen_titles:
+            seen_titles.add(title)
+            instructions = []  # Initialize a list to store the recipe instructions
+            for j in range(start_line + 5, len(recipe_list)):
+                if "Gerecht" in recipe_list[j] or j+1 == len(recipe_list):
+                    recipes.append({"title": title, "url": url, "amount": amount, "instructions": instructions})  # Store the title and instructions in a dictionary and append it to the list of recipes
                     break
-                elif recipe_num.isdigit() and int(recipe_num) in range(1, len(recipes)+1):
-                    # Write the recipe title and instructions to the file
-                    recipe = recipes[int(recipe_num)-1]
-                    file.write(f"{recipe['title']}:\n")
-                    for instruction in recipe['instructions']:
-                        file.write(f"\t{instruction}\n")
-                    print(f"{recipe['title']} has been added to your boodschappenlijst.")
                 else:
-                    print("Invalid input. Please enter the number of the recipe you want to add.")
+                    instructions.append(recipe_list[j].strip())  # Add the line to the instructions
+
+    # Print the found options
+    return recipes
+
+app = FastAPI()
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+
+
+@app.get('/')
+def ingredient_view(search_ingredient=None):
+    template = environment.get_template('index.html')
+
+    if search_ingredient:
+        recipe_list = find_ingredient(search_ingredient)
+    else:
+        recipe_list = []
+
+    content = template.render(
+        recipes=recipe_list,
+        search_ingredient=search_ingredient
+    )
+
+    return HTMLResponse(
+        content=content,
+        status_code=200,
+    )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
